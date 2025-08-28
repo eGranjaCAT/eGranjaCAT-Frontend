44c971a2aea290e24e838278a9808f608a589ec6
Yocmar Rafael Jiménez González <yocmarjigon@gmail.com>
2025-08-28 18:16:05 -0500

    Completada la maquetación de la pagina de login
    Falta la funcionalidad de cambiar el idioma y tambien la de loggear al los usuarios

--- src/app/pages/private/login-page/login-page.component.ts
+++ src/app/pages/private/login-page/login-page.component.ts
@@ -14,6 +14,9 @@ import { HlmInput } from '@spartan-ng/helm/input';
 import { FarmFormComponent, Granja } from '../farm-form/farm-form.component';
 import { HlmFormFieldModule } from '@spartan-ng/helm/form-field';
 import { HlmError } from '@spartan-ng/helm/form-field';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideEye, lucideEyeOff, lucideLock, lucideMail } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-login-page',
@@ -31,7 +34,18 @@ import { HlmError } from '@spartan-ng/helm/form-field';
     HlmInput,
     FarmFormComponent,
     HlmFormFieldModule,
-    HlmError
+    HlmError,
+    HlmIcon,
+    NgIcon,
+
+  ],
+  providers: [
+    provideIcons({
+      lucideMail,
+      lucideLock,
+      lucideEye,
+      lucideEyeOff
+    }),
   ],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
@@ -40,7 +54,7 @@ export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
   private router = inject(Router);
-
+  
   public loginForm = signal<FormGroup>(this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -48,12 +62,20 @@ export class LoginPageComponent {
 
   public statusButtonLanguage = signal(false);
 
+  // Flag para mostrar/ocultar la contraseña
+  public mostrarContrasena = signal(false);
+  // Flag para mostrar el formulario de granja
+  public mostrarFormGranja = signal(false);
+
+  // Método para alternar la visibilidad de la contraseña
+  public toggleMostrarContrasena(): void {
+    this.mostrarContrasena.set(!this.mostrarContrasena());
+  }
+
   public toggleLanguage(): void {
     this.statusButtonLanguage.set(!this.statusButtonLanguage());
   }
 
-  // Flag para mostrar el formulario de granja
-  public mostrarFormGranja = signal(false);
 
   // Acción al pulsar el botón Ingresar
   public onSubmit(): void {
@@ -65,5 +87,9 @@ export class LoginPageComponent {
     console.log('Granja guardada:', granja);
     this.mostrarFormGranja.set(false); // cerrar el formulario tras guardar
     this.router.navigate(['/private/dashboard-page']); // ahora sí navegamos
+  }
+
+  public submitLogin(): void {
+    this.router.navigate(['/private/dashboard-page']);
   }
 }
