65219b25ce5527baa86ec237017a5b7dc1ba60f0
Yocmar Rafael Jiménez González <yocmarjigon@gmail.com>
2025-08-28 19:38:43 -0500

    Avances en el header

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HlmMenu, HlmMenuGroup, HlmMenuItem, HlmMenuItemSubIndicator, HlmMenuLabel, HlmMenuSeparator, HlmMenuShortcut, HlmSubMenu } from '@spartan-ng/helm/menu';
+import { BrnMenuTrigger } from '@spartan-ng/brain/menu';
+import { HlmButton } from '@spartan-ng/helm/button';
+import { HlmSeparator } from '@spartan-ng/helm/separator';
+import { BrnSeparator } from '@spartan-ng/brain/separator';
+
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
   standalone: true,
   imports: [
- ]
+    HlmMenu,
+    HlmMenuGroup,
+    HlmMenuItem,
+    BrnMenuTrigger,
+    HlmMenuItem,
+    HlmMenuLabel,
+    HlmMenuSeparator,
+    HlmMenuGroup,
+    HlmButton,
+    HlmSeparator,
+
+  ],
+
 })
 export class HeaderBarComponent {
 
